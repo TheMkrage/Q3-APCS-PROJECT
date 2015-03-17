5db8dee2c76b0bedc25191bf10c4c335c77305d2
Matthew Krager <matthew.krager@STU-MKRAGER.student.friarsnet.servitehs.org>
2015-03-17 10:06:30 -0700

    pos/neg example

---
+++ src/org/q3project/operators/unary/UnaryPosNegExample.java
@@ -0,0 +1,13 @@
+package org.q3project.operators.unary;
+
+public class UnaryPosNegExample {
+	public static void main(String[] args) {
+
+		// negative and positive operators
+		int pos = +5;
+		int neg = -5;
+
+		System.out.println("POS " + pos);
+		System.out.println("NEG " + neg);
+	}
+}
