65b8fede043ace5287a08f13d6c7593c2fe9dc2b
Matthew Krager <matthew.krager@STU-MKRAGER.student.friarsnet.servitehs.org>
2015-03-17 09:58:31 -0700

    Assignment Operators
    Signed-off-by: Matthew Krager <matthew.krager@STU-MKRAGER.student.friarsnet.servitehs.org>

--- src/org/q3project/operators/AssignementExample.java
+++ src/org/q3project/operators/AssignementExample.java
@@ -1,5 +1,24 @@
 package org.q3project.operators;
 
 public class AssignementExample {
+	public static void main(String[] args) {
 
+		// = operator
+		int x = 5;
+
+		// + and * operators
+		int y = x + 3 * 2;
+
+		// - and / operators
+		int z = (y - 1) / 7;
+
+		// % operator
+		int w = 5 % 2;
+
+		// String concatenation operator +
+		System.out.println("x : " + x);
+		System.out.println("y : " + y);
+		System.out.println("z : " + z);
+		System.out.println("w : " + w);
+	}
 }
